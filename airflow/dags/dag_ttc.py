a5e6bff26f0cf6ee805ebfbaf21caa6b83034a0f
Roberto Almeida <scorpions2007@hotmail.com>
2024-02-19 18:52:38 -0300

    Update dag_ttc.py

--- airflow/dags/dag_ttc.py
+++ airflow/dags/dag_ttc.py
@@ -36,7 +36,7 @@ default_args = {
 
 # Definindo a DAG
 dag = DAG(
-    'dag_ttc',
+    'dag_ttc1',
     default_args=default_args,
     description='Uma DAG simples que consome dados de uma API e escreve em um arquivo',
     schedule_interval=timedelta(days=1),
